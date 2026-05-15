87be695edc114cf3198e1901dbd21ebbd6d740ae
Santiago Bernal <santiago.ramirezb1@uqvirtual.edu.co>
2026-05-15 15:49:58 -0500

    Inicio del proyecto
    Se agrega la estructura básica del proyecto TurismoRural: archivos .gitignore y .gitattributes, scripts de Maven Wrapper (mvnw, mvnw.cmd), pom.xml configurado con Spring Boot 4.0.6 (Java 21) y dependencias (spring-boot-starter-web, lombok, spring-boot-starter-test), y una clase principal básica TurismoRuralApplication.

---
+++ src/main/java/com/proyecto/TurismoRural/TurismoRuralApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto.TurismoRural;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurismoRuralApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TurismoRuralApplication.class, args);
+	}
+
+}
