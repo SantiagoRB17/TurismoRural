88792bd1bf83f9007b880308fdfad3216b154498
Santiago Bernal <santiago.ramirezb1@uqvirtual.edu.co>
2026-05-16 09:57:47 -0500

    T

---
+++ src/test/java/com/proyecto/TurismoRural/TurismoRuralApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.TurismoRural;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TurismoRuralApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
